f9a151297a8ed524e2a1e3b7e2666075bdaf0766
wangy <3084003139@qq.com>
2026-03-18 18:11:17 +0800

    添加注释

--- include/Core/FFmpegCommon.hpp
+++ include/Core/FFmpegCommon.hpp
@@ -16,17 +16,21 @@ namespace ff {
 
 std::string errStr(int err);
 
+//packet删除器
 struct PacketDeleter {
     void operator()(AVPacket* p) const;
 };
 
+//frame删除器
 struct FrameDeleter {
     void operator()(AVFrame* f) const;
 };
 
+//pakcet和frame的智能指针
 using PacketPtr = std::shared_ptr<AVPacket>;
 using FramePtr = std::shared_ptr<AVFrame>;
 
+//分配内存
 PacketPtr makePacket();
 FramePtr makeFrame();
 
